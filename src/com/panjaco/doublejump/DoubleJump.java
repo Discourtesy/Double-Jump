8e968e1e08474a6b13fa9ea374e67a362ce8804e
iPanja <fhenneman@comcast.net>
2016-09-04 01:16:00 -0400

    Uploaded plugin for the first time.

---
+++ src/com/panjaco/doublejump/DoubleJump.java
@@ -0,0 +1,29 @@
+package com.panjaco.doublejump;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DoubleJump extends JavaPlugin{
+
+	public void onEnable(){
+		PluginDescriptionFile descFile = getDescription();
+		Logger logger = getLogger();
+		logger.info("[Double Jump] Enabled");
+		
+		//Register listeners
+		Bukkit.getPluginManager().registerEvents(new onPlayerMovement(), this);
+		
+	}
+	
+	public void onDisable(){
+		
+		PluginDescriptionFile descFile = getDescription();
+		Logger logger = getLogger();
+		logger.info("[Double Jump] Disabled");
+		
+	}
+	
+}
